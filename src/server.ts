725e70ddab0156c4c67bcdda51273add30a8c562
mtyiska <mtyiska@gmailcom>
2022-03-31 06:24:02 -0400

    step2

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,30 @@
-// import http from "http";
+import http from "http";
 import express from "express";
 import { applyMiddleware, applyRoutes } from "./utils";
 import middleware from "./middleware";
+import errorHandlers from "./middleware/errorHandlers";
 import routes from "./services";
 
-const app = express();
-applyMiddleware(middleware, app);
-applyRoutes(routes, app);
+process.on("uncaughtException", e => {
+    console.log(e);
+    process.exit(1);
+});
+process.on("unhandledRejection", e => {
+    console.log(e);
+    process.exit(1);
+});
+
+const router = express();
+applyMiddleware(middleware, router);
+applyRoutes(routes, router);
+applyMiddleware(errorHandlers, router);
 
 const { PORT = 3000 } = process.env;
-// const server = http.createServer(app);
-// server.listen(PORT, () =>
-//   console.log(`Server is running http://localhost:${PORT}...`)
-// );
+const server = http.createServer(router);
+server.listen(PORT, () =>
+  console.log(`Server is running http://localhost:${PORT}...`)
+);
 
-app.listen(PORT, () =>{
-    console.log(`Server is running http://localhost:${PORT}...`)
-})
+// router.listen(PORT, () =>{
+//     console.log(`Server is running http://localhost:${PORT}...`)
+// })
